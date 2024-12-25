92e57364ad46f628014b80b2ae241f93d46d04d9
kisisellhesap <ahmetcannkaragoz@gmail.com>
2024-12-25 17:25:24 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
-import router from "./router/Router";
+import router from "./router/router";
 const App = () => {
   return <RouterProvider router={router} />;
 };
