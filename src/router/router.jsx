dc10b5ef40b068a705e330f9564a6a62c7333098
kisisellhesap <ahmetcannkaragoz@gmail.com>
2024-12-25 17:52:21 +0300

    update

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
         children: [
-          { path: "", element: <ProductList /> },
+          { path: "/", element: <ProductList /> },
           { path: ":category", element: <ProductList /> },
         ],
       },
